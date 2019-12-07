544ced4b14a15688a13ebd9b4bd0610e89510863
Gary Hughes <garyedwardhughes@gmail.com>
2019-12-07 15:17:30 +1100

    Add jest tests.

---
+++ src/components/Button/Button.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM, { unmountComponentAtNode } from 'react-dom'
+import renderer from 'react-test-renderer';
+import { Button } from '.';
+
+describe('Button', () => {
+
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render( < Button / > , div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
+
+    test('has a valid snapshot', () => {
+        const component = renderer.create( < Button / > );
+        const tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+
+});
